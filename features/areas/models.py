b12b7496bc41eab3a52d1e4740358b7f8752a2a5
Andrew Bastien <volundmush@gmail.com>
2019-11-18 17:59:12 -0500

    Beginning work on Areas.

--- features/areas/models.py
+++ features/areas/models.py
@@ -9,7 +9,10 @@ class AreaDB(TypedObject):
 
     db_parent = models.ForeignKey('self', null=True, on_delete=models.PROTECT, related_name='children')
     db_owner = models.ForeignKey('core.EntityMapDB', related_name='territory', null=True, on_delete=models.SET_NULL)
-    db_room_typeclass = models.ForeignKey('core.TypeclassMap', null=True, related_name='areas', on_delete=models.PROTECT)
+    db_room_typeclass = models.ForeignKey('core.TypeclassMap', null=True, related_name='areas',
+                                          on_delete=models.PROTECT)
+    db_exit_typeclass = models.ForeignKey('core.TypeclassMap', null=True, related_name='exits',
+                                          on_delete=models.PROTECT)
     db_fixtures = models.ManyToManyField('objects.ObjectDB', related_name='areas')
     db_transient = models.ManyToManyField('objects.ObjectDB', related_name='current_areas')
 
