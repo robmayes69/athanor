b856cc72de3cb124b4243528cb739809ebd16207
Andrew Bastien <volundmush@gmail.com>
2020-01-10 06:36:20 -0500

    Cleaning up Athanor big time!

--- athanor/building/models.py
+++ athanor/building/models.py
@@ -1,20 +1,21 @@
 from django.db import models
-from evennia.typeclasses.models import SharedMemoryModel
 
 
-class InstanceBridge(SharedMemoryModel):
-    db_object = models.OneToOneField('objects.ObjectDB', related_name='instance_bridge', primary_key=True,
+class InstanceBridge(models.Model):
+    object = models.OneToOneField('objects.ObjectDB', related_name='instance_bridge', primary_key=True,
                                      on_delete=models.CASCADE)
-    db_extension = models.CharField(max_length=255, null=False, blank=False)
-    db_instance_key = models.CharField(max_length=255, null=False, blank=False)
+    extension = models.CharField(max_length=255, null=False, blank=False)
+    instance_key = models.CharField(max_length=255, null=False, blank=False)
 
 
-class GameLocation(SharedMemoryModel):
-    db_object = models.OneToOneField('objects.ObjectDB', related_name='location_bridge', primary_key=True,
-                                     on_delete=models.CASCADE)
-    db_instance = models.ForeignKey('objects.ObjectDB', related_name='objects_here', on_delete=models.SET_NULL,
-                                    null=True)
-    db_room_key = models.CharField(max_length=255, null=True, blank=False)
-    db_x_coordinate = models.FloatField(null=True)
-    db_y_coordinate = models.FloatField(null=True)
-    db_z_coordinate = models.FloatField(null=True)
+class GameLocations(models.Model):
+    object = models.ForeignKey('objects.ObjectDB', related_name='saved_locations', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, null=False, blank=False)
+    instance = models.ForeignKey('objects.ObjectDB', related_name='objects_here', on_delete=models.CASCADE)
+    room_key = models.CharField(max_length=255, null=False, blank=False)
+    x_coordinate = models.FloatField(null=True)
+    y_coordinate = models.FloatField(null=True)
+    z_coordinate = models.FloatField(null=True)
+
+    class Meta:
+        unique_together = (('object', 'name'),)
--- athanor/core/gameentity.py
+++ athanor/core/gameentity.py
@@ -9,44 +9,88 @@ from athanor.items.handlers import GearHandler, InventoryHandler
 from athanor.factions.handlers import FactionHandler, AllianceHandler, DivisionHandler
 from athanor.core.handler import KeywordHandler
 from athanor.utils.color import green_yellow_red, red_yellow_green
-
+from athanor.building.handlers import LocationHandler
 
 class AthanorGameEntity(EventEmitter):
     persistent = False
 
     @lazy_property
-    def __location(self):
-        return None
+    def locations(self):
+        return LocationHandler(self)
+
+
 
     # location getsetter
     def __location_get(self):
         """Get location"""
-        return self.__location
+        return self.__room
 
-    def __location_set(self, location):
-        if location and not hasattr(location, 'instance'):
-            raise ValueError(f"{location} is not a valid location for a game entity.")
-        if (current_location := self.__location):
-            current_location.contents.remove(self)
-            current_location.instance.entities.remove(self)
-        if (self.__location := location):
-            location.contents.add(self)
-            location.instance.entities.add(self)
-        self.save_location(location)
+    def __location_set(self, room):
+        if room and not hasattr(room, 'instance'):
+            raise ValueError(f"{room} is not a valid location for a game entity.")
+        current_instance = None
+        if (current_location := self.__room):
+            current_instance = current_location.instance
+            current_location.unregister_entity(self)
+        if (self.__room := room):
+            if room.instance != current_instance:
+                room.instance.register_entity(self)
+            room.register_entity(self)
+        else:
+            if current_instance:
+                current_instance.unregister_entity(self)
+        if self.persistent:
+            self.save_location()
 
     def __location_del(self):
         """Cleanly delete the location reference"""
-        self.__location = None
+        instance = self.__instance
+        room = self.__room
+        self.__instance = None
+        self.__room = None
+        if instance:
+            self.__instance.unregister_entity(self)
+        if room:
+            self.__room.unregister_entity(self)
+        self.x = None
+        self.y = None
+        self.z = None
 
     location = property(__location_get, __location_set, __location_del)
 
-    def save_location(self, new_location):
-        if self.persistent:
-            loc_bri = self.location_bridge
-            loc_bri
+    def save_location(self):
+        loc_bri = self.location_bridge
+        loc_bri.instance = self.__instance
+        loc_bri.room_key = self.__room.room_key if self.__room else None
+        loc_bri.x_coordinate = self.x
+        loc_bri.y_coordinate = self.y
+        loc_bri.z_coordinate = self.z
+        loc_bri.save(update_fields=['instance', 'room_key', 'x_coordinate', 'y_coordinate', 'z_coordinate'])
+
+    def register_entity(self, entity):
+        if entity in self.entities:
+            return
+        self.entities.add(entity)
+        self.at_register_entity(entity)
+
+    def at_register_entity(self, entity):
+        pass
+
+    def unregister_entity(self, entity):
+        if entity not in self.entities:
+            return
+        self.entities.remove(entity)
+        self.at_unregister_entity(entity)
+
+    def at_unregister_entity(self, entity):
+        pass
 
     @lazy_property
     def contents(self):
+        return set()
+
+    @lazy_property
+    def entities(self):
         return set()
 
     @lazy_property
