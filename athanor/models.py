cd5844f29455e95f99051e2b0d9b94db91ad4915
Andrew Bastien <volundmush@gmail.com>
2020-05-02 17:33:05 -0700

    Working on new Models.

--- athanor/models.py
+++ athanor/models.py
@@ -254,53 +254,27 @@ class ChannelSubscription(SharedMemoryModel):
                            ('db_channel', 'db_icodename'))
 
 
-class DimensionDB(TypedObject):
-    """
-    Unlike the others, Dimensions are ALWAYS Fixtures. A Dimension should never be created unless save for
-    by the Asset load process.
-    """
-    __settingsclasspath__ = settings.BASE_DIMENSION_TYPECLASS
-    __defaultclasspath__ = "athanor.dimensions.dimensions.DefaultDimension"
-    __applabel__ = "athanor"
-
-    # db_key is treated as case-insensitively unique for this table.
-    # Store the plain text version of a name in this field.
-    db_name = models.CharField(max_length=255, null=False, blank=False)
-    # Store the version with Evennia-style ANSI markup in here.
-    db_cname = models.CharField(max_length=255, null=False, blank=False)
-
-
-class SectorDB(TypedObject):
-    __settingsclasspath__ = settings.BASE_SECTOR_TYPECLASS
-    __defaultclasspath__ = "athanor.sectors.sectors.DefaultSector"
-    __applabel__ = "athanor"
-
-    # db_key is treated as case-insensitively unique per-dimension for this table, per-dimension.
-    # Using a pre-fix for runtime-generated Sectors is advisable.
-
-    db_dimension = models.ForeignKey(DimensionDB, related_name='sectors', on_delete=models.PROTECT)
-    # Store the plain text version of a name in this field.
-    db_name = models.CharField(max_length=255, null=False, blank=False)
-    # Store the version with Evennia-style ANSI markup in here.
-    db_cname = models.CharField(max_length=255, null=False, blank=False)
-
-    # Do I want to use a Vector2 or a Vector3 for the coordinates of a Sector in a dimension?
-    # I suppose you could always ignore the Z...
-
-
 class EntityDB(TypedObject):
     """
-    This is Athanor's replacement to Evennia's ObjectDB.
+    This is Athanor's replacement to Evennia's ObjectDB. It is the core of an Entity-Component System-inspired approach.
     """
     __settingsclasspath__ = settings.BASE_ENTITY_TYPECLASS
     __defaultclasspath__ = "athanor.entities.entities.DefaultEntity"
     __applabel__ = "athanor"
 
-    # db_key must be case-insensitively unique globally. This isn't a name, it's an identifier.
-    # Use a pre-fix for runtime-generated stuff.
+
+class NameComponent(SharedMemoryModel):
+    db_entity = models.OneToOneField(EntityDB, related_name='command_component', on_delete=models.CASCADE)
     db_name = models.CharField(max_length=255, null=False, blank=False)
     # Store the version with Evennia-style ANSI markup in here.
     db_cname = models.CharField(max_length=255, null=False, blank=False)
+
+
+class CommandComponent(SharedMemoryModel):
+    """
+    Some entities might need to have CmdSets. Some might not. The storage is handled here.
+    """
+    db_entity = models.OneToOneField(EntityDB, related_name='command_component', on_delete=models.CASCADE)
 
     db_cmdset_storage = models.CharField(
         "cmdset",
@@ -310,141 +284,84 @@ class EntityDB(TypedObject):
         help_text="optional python path to a cmdset class.",
     )
 
-    inventories = GenericRelation('athanor.InventoryDB', related_name='entities')
-    equipsets = GenericRelation('athanor.EquipDB', related_query_name='entities')
 
-
-class SectorLocation(SharedMemoryModel):
-    db_sector = models.ForeignKey(SectorDB, related_name='sector_contents', on_delete=models.PROTECT)
-    db_entity = models.ForeignKey(EntityDB, related_name='sector_locations', on_delete=models.CASCADE)
-
-    # need some kind of vector3 here probably. Again, you could ignore the Z if you really want to I guess?
-    # Don't prevent two things from being in the same 'coordinates.' Code logic should handle that, not database.
-
-    class Meta:
-        # It probably doesn't make sense for the same entity to exist in more than one Sector. However, maybe for
-        # SOME things, it might? You would never want it to be in the same place twice, though.
-        unique_together = (('db_sector', 'db_entity'),)
-
-
-class RoomDB(TypedObject):
-    __settingsclasspath__ = settings.BASE_ROOM_TYPECLASS
-    __defaultclasspath__ = "athanor.rooms.rooms.DefaultRoom"
-    __applabel__ = "athanor"
-
-    # db_key is used as a unique-key for identifying this Room within a particular Entity.
-    # db_key is used for database exports and generating structures with consistent results.
-    # If new rooms are created in play, be careful to ensure they have a prefix.
-    # something like cust_whatever. This will prevent conflicts.
-    db_entity = models.ForeignKey(EntityDB, related_name='rooms', on_delete=models.CASCADE)
-    # Store the plain text version of the room's display name in this field.
-    db_name = models.CharField(max_length=255, null=False, blank=False)
-    # Store the version with Evennia-style ANSI markup in here.
-    db_cname = models.CharField(max_length=255, null=False, blank=False)
-    # all rooms belong to a given Entity. The Entity is the namespace for a Room.
-
-    class Meta:
-        unique_together = (('db_entity', 'db_key'),)
-
-
-class ExitDB(TypedObject):
-    __settingsclasspath__ = settings.BASE_EXIT_TYPECLASS
-    __defaultclasspath__ = "athanor.exits.exits.DefaultExit"
-    __applabel__ = "athanor"
-
-    # db_key is used as a unique-key for identifying this Room within a particular Entity.
-    # Store the plain text version of the room's display name in this field.
-    db_name = models.CharField(max_length=255, null=False, blank=False)
-    # Store the version with Evennia-style ANSI markup in here.
-    db_cname = models.CharField(max_length=255, null=False, blank=False)
-    # all rooms belong to a given Entity. The Entity is the namespace for a Room.
-    db_room = models.ForeignKey(RoomDB, related_name='exits', on_delete=models.CASCADE)
-    db_destination = models.ForeignKey(RoomDB, related_name='entrances', on_delete=models.SET_NULL, null=True)
-    db_pair = models.ForeignKey('self', related_name='linked_exits', on_delete=models.SET_NULL, null=True)
-
-    class Meta:
-        unique_together = (('db_room', 'db_key'),)
-
-
-class RoomLocation(SharedMemoryModel):
-    db_room = models.ForeignKey(RoomDB, related_name='room_contents', on_delete=models.PROTECT)
-    # Maybe db_entity should be a one-to-one-field?
-    db_entity = models.ForeignKey(EntityDB, related_name='room_locations', on_delete=models.CASCADE)
-
-    # Can you be in more than room at once? Um. Hrm. Maybe...
-    # Definitely not twice in the same place, though.
-    class Meta:
-        unique_together = (('db_room', 'db_entity'),)
-
-
-class InventoryDB(TypedObject):
+class FixtureComponent(SharedMemoryModel):
     """
-    There can be many different kinds of inventories - a shopkeeper's personal inventory and stock may be
-    different things. A Capital Ship might have a cargo bay and a fuel hangar. Given this, Inventories are
-    TypedObjects.
+    Fixtures are Entities which must exist - they are created by the load process, and integrity checked on server
+    start. This table is used for indexing and checking for an Entity's existence.
     """
-    __settingsclasspath__ = settings.BASE_INVENTORY_TYPECLASS
-    __defaultclasspath__ = "athanor.inventories.inventories.DefaultInventory"
-    __applabel__ = "athanor"
+    db_entity = models.OneToOneField(EntityDB, related_name='fixture_component', on_delete=models.CASCADE)
+    db_namespace = models.ForeignKey(Namespace, related_name='entity_fixtures', on_delete=models.PROTECT)
+    db_key = models.CharField(max_length=255, null=False, blank=False, unique=True)
 
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    owner_object = GenericForeignKey('content_type', 'object_id')
-    # db_key is used to identify the type of inventory it is specific to its owner. It must be unique per-entity.
+
+class InventoryComponent(SharedMemoryModel):
+    """
+    Component for entities which ARE an Inventory. Inventories have an Owner, which could literally be 'anything.'
+    This allows Factions to have Inventories, or Accounts, etc.
+    """
+    db_entity = models.OneToOneField(EntityDB, related_name='inventory_component', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=False)
+    object_id = models.PositiveIntegerField(null=False)
+    db_owner = GenericForeignKey('content_type', 'object_id')
+    db_key = models.CharField(max_length=255, null=False, blank=False)
 
     class Meta:
         unique_together = (('content_type', 'object_id', 'db_key'),)
 
 
-class InventoryLocation(SharedMemoryModel):
-    db_inventory = models.ForeignKey(InventoryDB, related_name='inventory_contents', on_delete=models.CASCADE)
-    db_entity = models.ForeignKey(EntityDB, related_name='inventory_locations', on_delete=models.CASCADE)
-
-    # Not sure what other kind of information is useful here yet.
-    # probably some way to sort the stuff.
-
-    class Meta:
-        # If nothing else, the same item cannot be listed in the same inventory more than once.
-        unique_together = (('db_inventory', 'db_entity'), )
-
-
-class EquipDB(TypedObject):
+class InventoryLocationComponent(SharedMemoryModel):
     """
-    Like with Inventories, there may be many kinds of Equip sets. This might be anything from a paper-doll style
-    inventory to a spaceship's part slots or maybe slotting gems into an sword to give it magical powers.
-
-    This is powered by GenericForeignKey so that ANYTHING can equip Items.
+    Component for Entities which are IN an Inventory.
     """
-    __settingsclasspath__ = settings.BASE_EQUIP_TYPECLASS
-    __defaultclasspath__ = "athanor.equips.equips.DefaultEquip"
-    __applabel__ = "athanor"
+    db_entity = models.OneToOneField(EntityDB, related_name='inventory_location_component', on_delete=models.CASCADE)
+    db_inventory = models.ForeignKey(EntityDB, related_name='inventory_contents', on_delete=models.PROTECT)
 
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    owner_object = GenericForeignKey('content_type', 'object_id')
-    # db_key is used to identify the type of EquippedSet it is specific to its owner. It must be unique per-entity.
 
-    # Not sure what else is relevant to an Equipped set yet.
+class EquipComponent(SharedMemoryModel):
+    """
+    Component for entities which ARE an EquipSet. EquipSets like Inventories have an Owner, which could be 'anything.'
+    """
+    db_entity = models.OneToOneField(EntityDB, related_name='equip_component', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=False)
+    object_id = models.PositiveIntegerField(null=False)
+    db_owner = GenericForeignKey('content_type', 'object_id')
+    db_key = models.CharField(max_length=255, null=False, blank=False)
 
     class Meta:
         unique_together = (('content_type', 'object_id', 'db_key'),)
 
 
-class EquipLocation(SharedMemoryModel):
+class EquipLocationComponent(SharedMemoryModel):
     """
-    Simple table for holding information on what's equipped where, with database enforcement.
+    Component for Entities which are equipped by an EquipSet.
     """
-    db_equip = models.ForeignKey(EquipDB, related_name='equipped_things', on_delete=models.CASCADE)
-    db_entity = models.ForeignKey(EntityDB, related_name='equipped_locations', on_delete=models.CASCADE)
+    db_entity = models.OneToOneField(EntityDB, related_name='equip_location_component', on_delete=models.CASCADE)
+    db_equip = models.ForeignKey(EntityDB, related_name='equip_contents', on_delete=models.PROTECT)
 
-    # The slot is the 'name' of the equipment slot this thing exists in. 'head' or 'socket' or 'about' or whatever.
-    db_slot = models.CharField(max_length=80, null=False, blank=False)
+    # Should probably stick something in here about how the item is equipped. Slot? Layer?
 
-    # the layer is the number corresponding to the 'layer' of the item. Some old MUDs love their multi-player gear sets
-    # This could also be used for something like where in EVE Online you have an entire row of 'high power slots' for
-    # your important active lasers and etc.
 
-    db_layer = models.PositiveIntegerField(default=0, null=False, blank=False)
+class DimensionComponent(SharedMemoryModel):
+    """
+    Component for Entities which ARE a Dimension.
+    """
+    db_entity = models.OneToOneField(EntityDB, related_name='dimension_component', on_delete=models.CASCADE)
 
-    class Meta:
-        unique_together = (('db_equip', 'db_entity'), ('db_equip', 'db_slot', 'db_layer'))
+
+class Vector3LocationComponent(SharedMemoryModel):
+    """
+    Component for Entities that are located 'inside' another Entity in a 3-Dimensional Space.
+    """
+    db_entity = models.OneToOneField(EntityDB, related_name='vector3_location_component', on_delete=models.CASCADE)
+    db_location = models.ForeignKey(EntityDB, related_name='vector3_contents', on_delete=models.PROTECT)
+    db_x = models.FloatField(default=0.0, null=False, blank=False)
+    db_y = models.FloatField(default=0.0, null=False, blank=False)
+
+
+class SectorComponent(SharedMemoryModel):
+    """
+    Component for Entities which ARE a Sector.
+    """
+    db_entity = models.OneToOneField(EntityDB, related_name='sector_component', on_delete=models.CASCADE)
+
